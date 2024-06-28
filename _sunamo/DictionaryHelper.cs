956458a6f4f4c740f8fba65aedd4ebf1ad964d01
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-28 23:05:56 +0200

    converted public to internal in _sunamo

--- _sunamo/DictionaryHelper.cs
+++ _sunamo/DictionaryHelper.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 
 namespace SunamoPackageJson;
-public class DictionaryHelper
+internal class DictionaryHelper
 {
     #region AddOrCreate
     /// <summary>
@@ -16,7 +16,7 @@ public class DictionaryHelper
     /// <param name="sl"></param>
     /// <param name="key"></param>
     /// <param name="value"></param>
-    public static void AddOrCreate<Key, Value, ColType>(IDictionary<Key, List<Value>> dict, Key key, Value value,
+    internal static void AddOrCreate<Key, Value, ColType>(IDictionary<Key, List<Value>> dict, Key key, Value value,
         bool withoutDuplicitiesInValue = false, Dictionary<Key, List<string>> dictS = null)
     {
         var compWithString = false;
@@ -121,7 +121,7 @@ public class DictionaryHelper
         }
     }
 
-    public static void AddOrCreate<Key, Value>(IDictionary<Key, List<Value>> sl, Key key, Value value,
+    internal static void AddOrCreate<Key, Value>(IDictionary<Key, List<Value>> sl, Key key, Value value,
         bool withoutDuplicitiesInValue = false, Dictionary<Key, List<string>> dictS = null)
     {
         AddOrCreate<Key, Value, object>(sl, key, value, withoutDuplicitiesInValue, dictS);
