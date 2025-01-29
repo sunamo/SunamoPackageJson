2c284212a1df3757a75bb8745d47153196e24d03
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 22:09:47 +0100

    Remove another unused methods

--- _sunamo/CA.cs
+++ _sunamo/CA.cs
@@ -2,10 +2,5 @@ namespace SunamoPackageJson._sunamo;
 
 internal class CA
 {
-    internal static List<string> Trim(List<string> l)
-    {
-        for (var i = 0; i < l.Count; i++) l[i] = l[i].Trim();
-        return l;
-    }
 
 }
